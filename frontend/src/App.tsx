c3cc3d67bc1fa3639a850d790ba3bdad6d31bb14
M Kumaran <kumaran.veera@outlook.com>
2026-08-07 16:04:15 +0200

    prj creation

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+// import { useState, useEffect, useRef } from "react";
+// import { Events, WML } from "@wailsio/runtime";
+// import { GreetService } from "../bindings/changeme";
+
+// // Show the actual Wails version this project was generated against.
+// const wailsVersion = "v3.0.0-beta.3";
+
+function App() {
+  return <>here we are d</>;
+}
+
+export default App;
---
+++ frontend/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import wails from "@wailsio/runtime/plugins/vite";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    host: "127.0.0.1",
+    port: Number(process.env.WAILS_VITE_PORT) || 9245,
+    strictPort: true,
+  },
+  plugins: [react(), wails("./bindings"), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
